b1380b8b044b4dcd39221041af455a75aa59f3cc
gui26 <guilhermeb20@gmail.com>
2021-12-24 13:09:51 -0300

    mellhorias navbar

---
+++ src/componentes/header.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import './../styles/header.scss';
+import { Notification} from '../componentes/Notification';
+import { Menu } from './menu';
+import  {FaSearch}  from "react-icons/fa";
+import  {AiFillBell}  from "react-icons/ai";
+import  {BsFillCaretDownFill}  from "react-icons/bs";
+
+type Propriedades={
+    className: any;
+}
+
+
+export const Header = (props:Propriedades)=>{
+
+const [showmenu, setShowmenu] = useState(false);
+const[display, setDisplay]= useState('teste');
+const[notification,setNofication]= useState(false)
+
+
+const PegarClass  = ()=>{
+    setDisplay('teste2')
+}
+    
+const ShowM = () =>{
+    setShowmenu(true);
+     PegarClass();
+}
+const hiddemM = () =>{
+    setShowmenu(false)
+        
+}
+const SairdaArea = () =>{
+    setShowmenu(false)
+}
+
+const NotificationDisplay = () =>{
+    setNofication(true);
+}
+    return( 
+            <div className={props.className}>
+                
+                <div className="navbar">
+                    <h1 className="Logo">Netflix</h1>
+                        <nav className="links">
+                            <a href="#">Inicio</a>
+                            <a href="#">Séries</a>
+                            <a href="#">Filmes</a>
+                            <a href="#">Bombando</a>
+                            <a href="#">Minha lista</a>
+                            <a href="#">Navegar por idiomas</a>                        
+                        </nav>
+              </div>
+           
+                 <div className="header-area-profile">
+                    <FaSearch />
+                    <h4>Adulto</h4>
+                    <AiFillBell size={24} onMouseEnter={NotificationDisplay}/>
+                    <div className="img"></div>
+
+                    <div className="iconClicar" onClick={ShowM} >
+                     <BsFillCaretDownFill size={15} color="white" />
+                    </div>
+                  </div>
+
+                {notification? <Notification onMouseLeave={()=>{
+                    setNofication(false)
+                }}/>: null }  
+
+                {showmenu ? <Menu onMouseLeave={SairdaArea}/> : null}
+
+
+
+                
+           
+        </div>
+    )
+}
+export default Header;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,78 +1,46 @@
-import React, { useState, useRef, useLayoutEffect} from "react";
+import React, { useState, useRef, useLayoutEffect, useEffect} from "react";
 import  {FaPlay}  from "react-icons/fa";
 import  {BsFillInfoCircleFill}  from "react-icons/bs";
-import  {FaSearch}  from "react-icons/fa";
-import  {AiFillBell}  from "react-icons/ai";
-import  {BsFillCaretDownFill}  from "react-icons/bs";
-import { Menu } from "../componentes/menu";
 import  {Flatlist} from "../componentes/Flatlist";
-import { Notification } from "../componentes/Notification";
 import  {MdArrowBackIosNew}  from "react-icons/md";
 import  {MdOutlineArrowForwardIos}  from "react-icons/md";
 
 import Carousel from 'react-elastic-carousel';
+import Header from "../componentes/header";
 
 
 export function  Home(){
     
-const [showmenu, setShowmenu] = useState(false);
+const [NavBar, setNavBar] = useState('container-header');
 const[display, setDisplay]= useState('teste');
-const[notification,setNofication]= useState(false)
+const[notification,setNofication]= useState(false);
 
 
-const PegarClass  = ()=>{
-    setDisplay('teste2')
-}
-    
-const ShowM = () =>{
-    setShowmenu(true);
-     PegarClass();
-}
-const hiddemM = () =>{
-    setShowmenu(false)
-        
-}
-const SairdaArea = () =>{
-    setShowmenu(false)
+useEffect( ()=>{
+
+ const scrolllistener = () =>{
+        if (window.scrollY > 10) {
+            setNavBar('container-black');          
+        } else {
+        setNavBar('container-header');;
+        }
+    }
+   window.addEventListener('scroll', scrolllistener);
+ return () => {
+    window.removeEventListener('scroll', scrolllistener);
 }
 
-const NotificationDisplay = () =>{
-    setNofication(true);
-}
+},[])
+
+
+
 return(
 
         <>
            <div className="container">
-              <div className="navbar">
-                    <h1 className="Logo">Netflix</h1>
-                        <nav className="links">
-                            <a href="#">Inicio</a>
-                            <a href="#">Séries</a>
-                            <a href="#">Filmes</a>
-                            <a href="#">Bombando</a>
-                            <a href="#">Minha lista</a>
-                            <a href="#">Navegar por idiomas</a>                        
-                        </nav>
-              </div>
-           
-                 <div className="pro">
-                    <FaSearch />
-                    <h4>Adulto</h4>
-                    <AiFillBell size={24} onMouseEnter={NotificationDisplay}/>
-                    <div className="img"></div>
-
-                    <div className="iconClicar" onClick={ShowM} >
-                     <BsFillCaretDownFill size={15} color="white" />
-                    </div>
-                  </div>
-
-                {notification? <Notification onMouseLeave={()=>{
-                    setNofication(false)
-                }}/>: null }  
-
-                {showmenu ? <Menu onMouseLeave={SairdaArea}/> : null}
-
-{/*  passar propieda      */}
+               <Header className={NavBar}/>
+              
+{/*  passar propiedades      */}
 
                
         </div>
@@ -81,7 +49,7 @@ return(
               <div className="filme-principal">
               
                    <div className="container2" >
-                 
+                     <div className="gradient-to-bottom"></div>
                       <h1 className="Title">The Witcher 2021</h1>
                       <p className="Descripition">O mutante Geralt de Rívia é um caçador de monstros que<br/> 
                       luta para encontrar seu lugar num mundo onde as pessoas <br/>muitas vezes são mais 
